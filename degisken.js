8fed322ef929ad58f7f1f99ca140908620ed425a
Muhammet Selim Ferah <muhammetselimferah@hotmail.com>
2020-03-31 12:18:23 +0300

    operatorler

--- degisken.js
+++ degisken.js
@@ -2,3 +2,10 @@ var a =23;
 var b='Bilgisayar Kavramları';
 console.log(a);
 console.log(b);
+var kisi ={ 
+    isim: 'ali',
+    soyisim: 'Demir'
+};
+console.log(kisi);
+console.log(kisi.isim);
+console.log(kisi.soyisim);
---
+++ operatorler.js
@@ -0,0 +1,13 @@
+var a=10;
+console.log(a);
+console.log(a+15);
+var b='Bilgisayar';
+var c='Kavramları';
+console.log(b+' '+c);
+var x='13';
+console.log(x+5);
+var y= parseInt(x);
+console.log(y+5);
+var z = y.toString();
+console.log(z+5);
+console.log(13+22*11-3/4);
